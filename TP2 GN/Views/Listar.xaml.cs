9186aa44cdc65eb35a65478673a3d35e67a96ce9
Romina Avalos <rflorencia.avalos@gmail.com>
2024-12-11 20:03:32 -0300

    first

--- TP2 GN/Views/Listar.xaml.cs
+++ TP2 GN/Views/Listar.xaml.cs
@@ -24,5 +24,10 @@ namespace TP2_GN.Views
             InitializeComponent();
 
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
