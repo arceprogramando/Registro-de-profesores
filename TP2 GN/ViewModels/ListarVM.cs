fd5d4d259d0098e61085b46dcd5392c2c57a6f6f
Romina Avalos <rflorencia.avalos@gmail.com>
2024-12-12 09:22:38 -0300

    fix: sign up form

--- TP2 GN/ViewModels/ListarVM.cs
+++ TP2 GN/ViewModels/ListarVM.cs
@@ -14,48 +14,5 @@ namespace TP2_GN.ViewModels
     internal class ListarVM : ViewModelBase
     {
 
-        private ProfesorModel _profesorSeleccionado;
-
-        private readonly DB dataBase;
-        private ObservableCollection<ProfesorModel> _profesores;
-
-        public ICommand ListarCommand { get; }
-
-        public ListarVM()
-        {
-            dataBase = new DB();
-            ListarCommand = new RelayCommand(ListarProfesores);
-        }
-
-        public ProfesorModel Profesor
-        {
-            get => _profesorSeleccionado;
-            set
-            {
-                _profesorSeleccionado = value;
-                OnPropertyChanged(nameof(Profesor));
-            }
-        }
-
-        public ObservableCollection<ProfesorModel> Profesores
-        {
-            get => _profesores;
-            set
-            {
-                if (_profesores != value)
-                {
-                    _profesores = value;
-                    OnPropertyChanged(nameof(Profesores));
-                }
-            }
-
-        }
-
-        private void ListarProfesores(object parameter = null)
-        {
-            // Supongamos que tienes un método en dataBase que obtiene la lista de profesores
-            Profesores = new ObservableCollection<ProfesorModel>(dataBase.Get());
-        }
-
     }
 }
--- TP2 GN/Views/Modificar.xaml.cs
+++ TP2 GN/Views/Modificar.xaml.cs
@@ -24,5 +24,6 @@ namespace TP2_GN.Views
         {
             InitializeComponent();
         }
+
     }
 }
